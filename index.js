137c4400eadcde152336d0ef1677d7c6d36f2397
mdumar112233 <mdumarfaruk187326@gmail.com>
2022-04-26 20:58:42 +0600

    update request

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+app.use(cors());
+app.use(bodyParser.json())
+
 const { MongoClient } = require('mongodb');
+const { ObjectID } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij0ac.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
@@ -25,112 +26,45 @@ client.connect(err => {
   // const reviewsCollection = client.db("akIndustry").collection("reviews");
   // const adminCollection = client.db("akIndustry").collection("admin");
 
-  app.post('/appointment', async (req, res) => {
+  app.post('/appointment', (req, res) => {
     const appointment = req.body;
     console.log(appointment)
-    try {
-      const data = await appointmentCollection.insertOne(appointment)
-      res.send(data.insertedCount > 0)
-    } catch (error) {
-      console.log(error)
-    }
+    appointmentCollection.insertOne(appointment)
+    .then(result => {
+        res.send(result.insertedCount > 0);
+    })
   })
 
-  app.get('/getappointmentdata', (req, res) => {
-  appointmentCollection.find({})
+  app.patch('/update/:id', (req, res) => {
+    appointmentCollection.updateOne({_id: ObjectID(req.params.id)},
+    {
+      $set:{ status: req.body.status}
+    })
+    .then(result => {
+      console.log('updated')
+    })
+  })
+  
+  app.get('/getallappointment', (req, res) => {
+    appointmentCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+    })
+  app.get('/getuserappointment', (req, res) => {
+  appointmentCollection.find({email: req.query.email})
   .toArray((err, documents) => {
     res.send(documents)
   })
   })
 
-
-  // app.post('/review', (req, res) => {
-  //   const review = req.body;
-  //   reviewsCollection.insertOne(review)
-  //   .then(result => {
-  //     res.send(result.insertedCount > 0)
-  //   })
-  // })
-
-  // app.post('/userInfo', (req, res) => {
-  //   const userInfo = req.body;
-  //   userCollection.insertOne(userInfo)
-  //   .then(result => {
-  //     res.send(result.insertedCount > 0);
-  //   })
-    
-  // })
-
-  // app.post('/addAdmin', (req, res) => {
-  //   const admin = req.body;
-  //   adminCollection.insertOne(admin)
-  //   .then(result => {
-  //     res.send(result.insertedCount > 0);
-  //   })
-  // })
-
-  // app.patch('/update/:id', (req, res) => {
-  //   userCollection.updateOne({_id: ObjectID(req.params.id)},
-  //   {
-  //     $set:{ status: req.body.status}
-  //   })
-  //   .then(result => {
-  //     console.log(result);
-  //   })
-  // })
-
-
-  // app.get('/reviewData', (req, res)=> {
-  //   reviewsCollection.find({})
-  //   .toArray((err, documents) => {
-  //     res.send(documents)
-  //   })
-  // })
-
-  // app.get('/serviceInfo/:id', (req, res) => {
-  //   const id = req.params.id;
-  //   servicesCollection.find({name: id})
-  //   .toArray((err, documents) => {
-  //     res.send(documents[0])
-  //   })
-  // })
-
-  // app.get('/allUserData', (req, res) => {
-  //   userCollection.find({})
-  //   .toArray((err, documents) => {
-  //     console.log('data is come');
-  //     res.send(documents);
-  //   })
-  // })
-
-  // app.get('/status', (req, res) => {
-  //   userCollection.find({email: req.query.email})
-  //   .toArray((err, documents) => {
-  //     res.send(documents[0]);
-  //   })
-  // })
-
-  // app.get('/getAdmin', (req, res) => {
-  //   adminCollection.find({})
-  //   .toArray((err, documents) => {
-  //     res.send(documents)
-  //   })
-  // })
-
-  // app.delete('/delete/:id', (req, res) => {
-  //   servicesCollection.deleteOne({_id: ObjectID(req.params.id)})
-  //   .then(result => {
-  //     res.send(result.deletedCount > 0);
-  //   })
-  // })
-
+  app.delete('/delete/:id', (req, res) => {
+  appointmentCollection.deleteOne({_id: ObjectID(req.params.id)})
+  .then(result => {
+    res.send(result.deletedCount > 0);
+  })
+  })
 });
-
-
-
-
-
-
 app.listen(port)
 
 
